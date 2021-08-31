9f45cc1fb4d6ca0ddcb945860217efb12d8906dd
Enio N. de Souza <enionsouza@gmail.com>
2021-08-31 16:45:40 -0400

    Add HTML markup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
-import WebpackLogo from './img/webpack-logo.svg'
-
-const h1 = document.getElementsByTagName('h1')[0]
-h1.classList.add('new-style');
-
-const logo = document.createElement('img');
-logo.src = WebpackLogo;
-
-h1.appendChild(logo);
