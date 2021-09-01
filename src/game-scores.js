b6747c9bf3ef7a90aaa435c8993b87204598c152
Enio N. de Souza <enionsouza@gmail.com>
2021-09-01 18:52:36 -0400

    Implement form 'Submit' button

--- src/game-scores.js
+++ src/game-scores.js
@@ -1,11 +1,23 @@
 export default class {
   constructor(gameID) {
     this.gameID = gameID;
+    this.url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${this.gameID}/scores/`;
   }
 
   get scores() {
-    const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${this.gameID}/scores/`;
-    return fetch(url)
+    return fetch(this.url)
+      .then((res) => res.json())
+      .then((json) => json.result);
+  }
+
+  submitScore(user, score) {
+    fetch(this.url, {
+      method: 'POST',
+      body: JSON.stringify({ user, score }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
       .then((res) => res.json())
       .then((json) => json.result);
   }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import './style.css';
 import GameScores from './game-scores';
 import { gameID as id } from './create-new-game';
 
-let gameID;
-
 id.then((res) => {
   const regexp = /(Game with ID: )|( added.)/g;
-  gameID = localStorage.gameID || res.replace(regexp, '');
+  const gameID = localStorage.gameID || res.replace(regexp, '');
   if (!localStorage.gameID) localStorage.gameID = gameID;
 
   const game = new GameScores(gameID);
   const refreshBtn = document.querySelector('button');
+  const submitBtn = document.querySelector('input[type="submit"]');
 
   async function renderTable() {
     const tbody = document.querySelector('tbody');
@@ -27,6 +26,17 @@ id.then((res) => {
     });
   }
 
+  async function submitNewScore(e) {
+    e.preventDefault();
+    const user = document.getElementById('username');
+    const score = document.getElementById('score');
+    game.submitScore(user.value, score.value);
+    user.value = '';
+    score.value = '';
+    renderTable();
+  }
+
   refreshBtn.onclick = renderTable;
+  submitBtn.onclick = submitNewScore;
   window.onload = renderTable;
 });
